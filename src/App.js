0f004647ecd67dde5b82e97d2e03a4e59d5cee8a
sri venkatesh lp <lps.162@gmail.com>
2022-02-13 13:55:54 +0530

    react select

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
+import Select from 'react-select'
+
 function App() {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
   return (
     <div className="App">
       <h3>Super cool apps with React, Cloudflare</h3>
+      <div className='dropdown'>
+        <Select options={options} />
+      </div>
     </div>
   );
 }
